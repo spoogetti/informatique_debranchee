d403465cd9b28410e404b16eed220fda91103b49
Dumery <laurie@MacBook-Pro-de-Dumery.local>
2020-07-09 12:08:29 +0200

    Reorganize css

--- src/index.js
+++ src/index.js
@@ -3,39 +3,41 @@ import Phaser from "phaser";
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/main.css';
+import './assets/styles/home.css';
+import './assets/styles/about.css';
 
 import ReconnectGame from "../lib/ReconnectGame";
 
 document.querySelector("#startGameBtn").addEventListener("click", () => {
-  let pseudo = document.querySelector("#pseudo").value;
-  document.querySelector("#loginView").remove();
-  launchGame(pseudo)
+    let pseudo = document.querySelector("#pseudo").value;
+    document.querySelector("#loginView").remove();
+    launchGame(pseudo)
 })
 
 const launchGame = (pseudo) => {
 
-  let margins = 200
-  // setup container
-  document.querySelector("#app").style.width = window.innerWidth + "px"
-  document.querySelector("#app").style.height = window.innerHeight + margins + "px"
-  document.querySelector("#app").style.overflow = "scroll"
+    let margins = 200
+        // setup container
+    document.querySelector("#app").style.width = window.innerWidth + "px"
+    document.querySelector("#app").style.height = window.innerHeight + margins + "px"
+    document.querySelector("#app").style.overflow = "scroll"
 
-  const config = {
-    scale: {
-      parent: "app",
-      width: window.innerWidth,
-      height: window.innerHeight + margins,
-      mode: Phaser.Scale.ENVELOP,
-    },
-    scene: {
-      create: create
+    const config = {
+        scale: {
+            parent: "app",
+            width: window.innerWidth,
+            height: window.innerHeight + margins,
+            mode: Phaser.Scale.ENVELOP,
+        },
+        scene: {
+            create: create
+        }
+    };
+
+    var game = new ReconnectGame(config)
+
+    function create() {
+        game.createMap();
+        game.configurePlayer(pseudo)
     }
-  };
-  
-  var game = new ReconnectGame(config)
-  
-  function create() {
-    game.createMap();
-    game.configurePlayer(pseudo)
-  }
 }
