a9f5b67589a4e92cd6630c109ca5ebfb09a5113d
Moinet <pierre.moinet@acrib.fr>
2020-07-10 10:31:47 +0200

    feat(albert): dialogs for wron guesses

---
+++ src/dialogs.js
@@ -0,0 +1,14 @@
+const dialogs = {
+    wrongGuess: [
+        "Essaie encore !",
+        "Tu y es presque",
+        "Presque... Je suis sûr que tu peux le faire !",
+        "Ce n'est toujours pas ça",
+        "Persévére, tu vas y arriver !",
+        "Tu touches au but",
+        "Ce n'est pas encore ça.. Mais tu es sur la bonne voie !",
+        "Continue comme ça"
+    ]
+}
+
+export default dialogs
